846d0560db5f152ea5a0e28da12d74f5e9fc6b56
marekc <marek.caltik@solargis.com>
2023-03-07 11:50:40 +0100

    identity changed to v1.0.1 new files added - bundled schema versions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Solargis public JSON schema files define JSON instances of various request and response object types.
+
+#### RELEASE NOTES:
+- **v1.0.1** (Mar 7 2023): Column names refactored, new UncertaintyColumnTypesEnum added, groundAlbedo renamed to surfaceAlbedo. RequestType is not required for public requests.
+- **v1.0.0** (Feb 16 2023): Redefined request property utcOffset from int to string e.g, '9' changed to '+09:00'.
---
+++ change_schema_identity.py
@@ -0,0 +1,25 @@
+import glob
+import os
+from pathlib import Path
+
+old_identity = '/v1.0.1/'
+new_identity = '/v1.0.1/v1/'
+
+here = Path(__file__).parent
+schema_folder = str(here / "v1")
+
+file_list = glob.glob(os.path.join(schema_folder, "*.schema.json"))
+
+for filename in file_list:
+    with open(filename, "r") as file:
+        file_content = file.read()
+        # Replace the text
+        new_content = file_content.replace(old_identity, new_identity)
+
+    # Open the file again in write mode
+    with open(filename, "w") as file:
+        # Write the new content to the file
+        file.write(new_content)
+    print('Updated %s.' % filename)
+
+print('All done.')
